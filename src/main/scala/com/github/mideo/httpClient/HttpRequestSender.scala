6b93cb558d70eb05598370d445bb7cfff54afe4c
Mide Ojikutu <mide.ojikutu@gmail.com>
2019-07-28 12:34:06 +0100

    wip

--- src/main/scala/com/github/mideo/httpClient/HttpRequestSender.scala
+++ src/main/scala/com/github/mideo/httpClient/HttpRequestSender.scala
@@ -4,6 +4,15 @@ import java.net.HttpURLConnection
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.mideo.httpClient.Implicits._
+import io.netty.bootstrap.Bootstrap
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioSocketChannel
+import io.netty.channel.{ChannelInitializer, EventLoopGroup}
+import io.netty.handler.codec.http
+import io.netty.handler.codec.http.{HttpClientCodec, HttpContentDecompressor, HttpUtil}
+import io.netty.handler.ssl.SslContextBuilder
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory
 
 import scala.annotation.tailrec
 import scala.collection.JavaConverters._
@@ -52,6 +61,39 @@ object HttpRequestSender {
     result
   }
 
+  def doNettySend[T, K: ClassTag](request: AbstractHttpRequest[T])(implicit objectMapper: ObjectMapper) = Try {
+
+
+    val port: Int = request.URL.toURI.getPort match {
+      case i if i == -1 && request.URL.toURI.getScheme.equalsIgnoreCase("https") => 443
+      case i if i == -1 && request.URL.toURI.getScheme.equalsIgnoreCase("http") => 80
+      case i => i
+    }
+
+    val group: EventLoopGroup = new NioEventLoopGroup()
+    val bootstrap: Bootstrap = new Bootstrap()
+
+    request.URL.toURI.getScheme match {
+      case s if s.equalsIgnoreCase("https") =>
+
+        bootstrap.group(group)
+          .channel(classOf[NioSocketChannel])
+          .handler(new ChannelInitializer[SocketChannel] {
+            override def initChannel(ch: SocketChannel): Unit = {
+              ch.pipeline
+                .addLast(SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build().newHandler(ch.alloc()))
+                .addLast(new HttpClientCodec())
+                .addLast(new HttpContentDecompressor())
+                .addLast(new HttpClientHandler())
+
+
+            }
+          })
+      case _ => _
+    }
+
+  }
+
   private def responseHeaders(implicit connection: HttpURLConnection): Map[String, String] = connection
     .getHeaderFields
     .keySet().asScala
@@ -67,4 +109,39 @@ object HttpRequestSender {
     }
   }
 
+
+}
+
+import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
+import io.netty.handler.codec.http.{HttpContent, HttpObject, LastHttpContent}
+import io.netty.util.CharsetUtil
+import collection.JavaConverters._
+class HttpClientHandler extends SimpleChannelInboundHandler[HttpObject] {
+  override def channelRead0(ctx: ChannelHandlerContext, msg: HttpObject): Unit = {
+    msg match {
+      case response: http.HttpResponse =>
+        if (!response.headers.isEmpty) {
+          response.headers.names().asScala.foreach(
+            name =>  response.headers.getAll(name).forEach(
+              value => System.err.println("HEADER: " + name + " = " + value)
+            )
+          )
+        }
+        if (HttpUtil.isTransferEncodingChunked(response)) System.err.println("CHUNKED CONTENT {")
+        else System.err.println("CONTENT {")
+      case content: HttpContent =>
+        System.err.print(content.content.toString(CharsetUtil.UTF_8))
+        System.err.flush()
+        if (content.isInstanceOf[LastHttpContent]) {
+          System.err.println("} END OF CONTENT")
+          ctx.close
+        }
+      case _ =>
+    }
+  }
+
+  override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = {
+    cause.printStackTrace()
+    ctx.close
+  }
 }
